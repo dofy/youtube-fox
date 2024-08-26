47a9477d7ddaafd9f95bea07dd50ebf0a60a7f3f
Seven Yu <dofyyu@gmail.com>
2024-08-26 23:24:24 +0800

    update ui

--- src/contentScript/Content.tsx
+++ src/contentScript/Content.tsx
@@ -1,9 +1,11 @@
-import { Button, useDisclosure } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import '/global.css'
 
 export const Content = () => {
   return (
-    <div>
-      <Button>Get Captions</Button>
+    <div className="flex gap-2 items-center">
+      <div className="bg-blue-100 rounded-md py-2 px-4">YouTube</div>
+      <div className="p-4">Get Captions</div>
     </div>
   )
 }
