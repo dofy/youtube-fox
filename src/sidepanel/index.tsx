1d430def69c54c822b0222c0f81e260a10fab308
Seven Yu <dofyyu@gmail.com>
2024-08-27 23:12:14 +0800

    fix remove chakra-ui

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { SidePanel } from './SidePanel'
@@ -6,8 +5,6 @@ import '/global.css'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <SidePanel />
-    </ChakraProvider>
+    <SidePanel />
   </React.StrictMode>,
 )
